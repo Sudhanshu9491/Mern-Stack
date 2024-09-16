1e5bfe96896aa027bd0c475c1cf43a5d56cbb15e
sudhanshu <sudhanshumishra9491@gmail.com>
2024-09-16 15:46:14 +0530

    creates pages and route using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-export default function App() {
+import React from 'react'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from './pages/Home'
+import SingIn from './pages/SingIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Sudhanshu Mishra
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home></Home>} ></Route>
+        <Route path='/sign-in' element={<SingIn></SingIn>} ></Route>
+        <Route path='/sign-up' element={<SignUp></SignUp>} ></Route>
+        <Route path='/about' element={<About></About>} ></Route>
+        <Route path='/profile' element={<Profile></Profile>} ></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
+
+export default App
